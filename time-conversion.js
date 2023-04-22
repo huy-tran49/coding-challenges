893747f9ebcfabf7c718d269a456065c15f95f7d
Huy Tran <h.qt193@gmail.com>
2023-04-22 11:22:17 -0700

    revise solution for time conversion

--- time-conversion.js
+++ time-conversion.js
@@ -5,8 +5,14 @@ function timeConversion(s) {
     let sec = s.slice(6,8)
     let pm = s.slice(8,10)
     let hours
-    if (pm == 'PM') {
+    if (hour < 12 && pm == 'PM') {
         hours = parseInt(hour) + 12
+    } else if (hour == 12 && pm == 'AM') {
+        hours = '00'
+    } else if (hour == 12 && pm == 'PM') {
+        hours = '12'
+    } else if (hour < 12 && pm == 'AM') {
+        hours = hour
     }
     const time = `${hours}:${min}:${sec}`
     return time
